46529aa0e18896d9cb4babe51205145dbf37b64b
유동균 <a67422830@gmail.com>
2026-03-29 23:14:33 +0900

    feat: user, event service 프로젝트 생성

---
+++ services/event-service/src/test/java/com/zerotrust/k8s/eventservice/EventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerotrust.k8s.eventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
